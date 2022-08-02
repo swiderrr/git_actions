77c11cfac3d3529fdf6807672007d784aead45d8
karol.swidrak <karol.swidrak@bluesoft.com>
2022-08-03 00:48:22 +0200

    Configured GitHub Actions Workflow

--- app2/consumer.py
+++ app2/consumer.py
@@ -13,14 +13,12 @@ def callback(ch, method, properties, body):
     except Exception as err:
         return err
 
-try:
-    credentials = pika.PlainCredentials(os.getenv('RABBITMQ_USER', 'guest'), password=os.getenv('RABBITMQ_PASSWORD', 'guest'))
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.getenv('RABBITMQ_HOST', 'localhost'), port=os.getenv('RABBITMQ_PORT', 5672), heartbeat=0, blocked_connection_timeout=300, credentials=credentials))
-    channel = connection.channel()
-    channel.queue_declare(queue=os.getenv('CONSUMER_QUEUE', 'blue'))
-    channel.basic_consume(queue=os.getenv('CONSUMER_QUEUE', 'blue'), on_message_callback=callback, auto_ack=True)
-except Exception as err_conn:
-    print (err_conn)
+credentials = pika.PlainCredentials(os.getenv('RABBITMQ_USER', 'guest'), password=os.getenv('RABBITMQ_PASSWORD', 'guest'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.getenv('RABBITMQ_HOST', 'localhost'), port=os.getenv('RABBITMQ_PORT', 5672), heartbeat=0, blocked_connection_timeout=300, credentials=credentials))
+channel = connection.channel()
+channel.queue_declare(queue=os.getenv('CONSUMER_QUEUE', 'blue'))
+channel.basic_consume(queue=os.getenv('CONSUMER_QUEUE', 'blue'), on_message_callback=callback, auto_ack=True)
+
 redis_client = redis.Redis(host=os.getenv('REDIS_HOST', 'localhost'), port=int(os.getenv('REDIS_PORT', '6379')), db=0)
 
 
